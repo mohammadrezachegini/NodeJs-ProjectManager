e1f46aed5b70631d4c33dc6e3da5141ecba47d0f
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-09 22:19:44 -0800

    change getMyTeam method add lookup to get data from object id and show as a array in json

--- app/http/controllers/team.controller.js
+++ app/http/controllers/team.controller.js
@@ -63,12 +63,38 @@ class TeamController{
     async getMyTeam(req,res,next){
         try {
             const userID = req.user._id;
-            const teams = await TeamModel.find({
-                $or: [
-                    {owner: userID},
-                    {users: userID}
-                ]
-            });
+            // const team = await TeamModel.findOne({
+            //     $or: [{ owner : userID}, {users: userID}]
+            // })
+            const teams = await TeamModel.aggregate([
+                {
+                    $match: {
+                        $or: [{owner: userID},{users: userID}]
+                    }
+                },
+                {
+                    $lookup: {
+                        from : "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner"
+                    }
+                },
+                    {
+                        $project: {
+                            "owner.roles" : 0,
+                            "owner.password" : 0,
+                            "owner.token" : 0,
+                            "owner.teams" : 0,
+                            "owner.skills" : 0,
+                            "owner.invitation" :0
+                        }
+                    },
+                    {
+                        $unwind: "$owner"
+                    }
+                
+            ]);
             return res.status(200).json({
                 status: 200,
                 success: true,
